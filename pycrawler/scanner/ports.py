d71f10e7fd8b627e4c273a25d300a65fc09883dc
Vitor Fernandes <vitor.fernandes@blip.pt>
2017-03-26 13:32:42 +0100

    replaced port dictionary

--- pycrawler/scanner/ports.py
+++ pycrawler/scanner/ports.py
@@ -5,7 +5,7 @@ class PortScanner():
 
     MAX_RANGE=65536
 
-    def __init__(self,server,ports=[],delay=0.01,timeout=0.1):
+    def __init__(self,server,ports={},delay=0.01,timeout=0.1):
         self.serverip=server
         self.knownports=ports
         self.delay=delay
@@ -13,15 +13,16 @@ class PortScanner():
 
     def scan(self):
         ports = []
+        k = self.knownports.keys()
         try:
-            for port in self.knownports:
+            for port in k:
                 time.sleep(self.delay)
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.settimeout(self.timeout)
-                result = sock.connect_ex((self.serverip, port))
+                result = sock.connect_ex((self.serverip, int(port)))
                 if result == 0:
                     print "Ip: {}, Port {}: 	 Open".format(self.serverip,port)
-                    ports.append(port)
+                    ports.append({port:self.knownports[port]})
                 else:
                     print "Ip: {}, Port {}: 	 Closed".format(self.serverip,port)
                 sock.close()
